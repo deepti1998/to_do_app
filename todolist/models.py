1a56249cf913244bc43e0e89383e7c4978490d8d
deepti1998 <deeptigupta1998@gmail.com>
2019-03-07 18:14:41 +0530

    Update models.py

--- todolist/models.py
+++ todolist/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -10,6 +11,7 @@ class ToDoList(models.Model):
     due_date = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))
     due_time = models.TimeField(default=timezone.now().time())
     completed = models.BooleanField(default=False)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.title
